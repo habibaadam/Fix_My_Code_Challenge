caf5f125b90034d8314a73438814eed57c804f31
habibaadam <habiepalmer@gmail.com>
2023-10-17 16:51:46 +0000

    3. User password

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -18,7 +18,7 @@ class User():
     def __init__(self):
         """
         Initialize a new user:
-        - assigned an unique `id`
+        - assigned an unique id
         """
         self.id = str(uuid.uuid4())
 
@@ -33,28 +33,28 @@ class User():
     def password(self, pwd):
         """
         Password setter:
-        - `None` if `pwd` is `None`
-        - `None` if `pwd` is not a string
-        - Hash `pwd` in MD5 before assign to `__password`
+        - None if pwd is None
+        - None if pwd is not a string
+        - Hash pwd in MD5 before assign to __password
         """
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
         Valid password:
-        - `False` if `pwd` is `None`
-        - `False` if `pwd` is not a string
-        - `False` if `__password` is `None`
-        - Compare `__password` and the MD5 value of `pwd`
+        - False if pwd is None
+        - False if pwd is not a string
+        - False if __password is None
+        - Compare __password and the MD5 value of pwd
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
